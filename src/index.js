4d7af816b0b391b141dda55b38cefb2e1331a1d9
Sarah Foster <sarah.foster0100@gmail.com>
2023-04-20 22:55:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//US-06: changed color of titles
+//US-06: 
+
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
@@ -16,20 +19,36 @@ function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// console.log("A random integer between 0 and 10");
+// console.log(randomInteger(0, 10));
+// console.log("Another random integer between 0 and 10");
+// console.log(randomInteger(0, 10));
+// console.log("A random number between 600 and 1200");
+// console.log(randomInteger(600, 1200));
+
+//followed repl 
 
 function setDelay(difficulty) {
   // TODO: Write your code here.
-
+ //console.log("setDelay")
     return 1000
   }
- 
-function chooseHole(holes) {
-  // TODO: Write your code here.
-  
-    const index = Math.floor(Math.random() * 2);
+  //setDelay("easy") //> Returns 1500
+  //setDelay("normal") //> Returns 1000
+  //setDelay("hard") //> Returns 856 (returns a random number between 600 and 1200).
+
+
+  function chooseHole(holes) {
+    //console.log("chooseHole")
+      // 1. Generate a random integer from 0 to 8 and assign it to an index variable.
+      // 2. Get a random hole with the random index (e.g., const hole = holes[index]).
+      // 3. if hole === lastHole, then call chooseHole(holes) again because you don't want to return the same hole.
+      // 4. if hole is not the same as the lastHole, then keep track of it (lastHole = hole) and return the hole.
+    
+    const index = Math.floor(Math.random() * 8);
     const hole = holes[index];
     if (hole === lastHole) {
-      return chooseHoles(holes);
+      return chooseHole(holes);
     }
     lastHole = hole;
     return hole;
@@ -39,16 +58,12 @@ function chooseHole(holes) {
    //hole.classList.toggle("show");
    //console.log(hole.innerHTML);
    //console.log(hole.classList);
- 
- //if time > 0:
-  //timeoutId = showUp()
-  //  return timeoutId
- //else
-//gameStopped = stopGame()
-//return gameStopped
 
+    //code above: This function should select a random hole from the list of holes that you defined. 
+    //When you call the function, you should make sure that it doesn't return the last hole.
 
 function gameOver() {
+ // console.log("gameOver");
   // TODO: Write your code here
   if(time > 0){
     let timeoutId = showUp();
@@ -58,8 +73,11 @@ function gameOver() {
     return gameStopped;
   }
 }
+//The purpose of this function is simply to determine if the game should continue or stop.
+
 
 function showUp() {
+ // console.log("showUp");
   let delay = setDelay("easy"); // TODO: Update so that it uses setDelay()
   const hole = chooseHole(holes);  // TODO: Update so that it use chooseHole()
   return showAndHide(hole, delay);
@@ -67,6 +85,7 @@ function showUp() {
 
 
 function showAndHide(hole, delay){
+  //console.log("showAndHide");
   toggleVisibility(hole);
   const timeoutID = setTimeout(() => {
     toggleVisibility(hole);
@@ -75,8 +94,19 @@ function showAndHide(hole, delay){
   return timeoutID;
 }
 
+//function showAndHide(hole, delay){
+  // TODO: Call the toggleVisibility() function so that it adds the show class.
+  
+  //const timeoutID = setTimeout(() => {
+    // TODO: Call the toggleVisibility() function so that it removes the show class when the timer times out.
+    
+   // gameOver();
+  //}, 0); // TODO: Change the setTimeout() delay to the one provided as a parameter
+  //return timeoutID;
+//}
 
 function toggleVisibility(hole){
+  //console.log("toggleVisibility");
   // TODO: add hole.classList.toggle so that it adds or removes the 'show' class.
   hole.classList.toggle('show');
   return hole;
@@ -85,14 +115,22 @@ function toggleVisibility(hole){
 //showUp ();
 
 function updateScore() {
- 
-  console.log(points);
-  //score.textContent = points;
-  //return points;
+ console.log("updateScore", points);
+ score.textContent = points++; 
+ return points;
 }
 
-function clearScore() {
-  // TODO: Write your code here
+  // Increment the points global variable by 1 point
+  // Update score.textContent with points.
+  // Return points;
+  
+
+  function clearScore() {
+    //console.log("clearScore");
+    // set the points global variable to 0
+    // update score.textContent 
+    // return points;
+  
   points = 0;
   score.textContent = points;
   return points;
@@ -100,6 +138,7 @@ function clearScore() {
 
 
 function updateTimer() {
+  //console.log("updateTimer");
   // TODO: Write your code here.
   // hint: this code is provided to you in the instructions.
   if (time > 0){
@@ -111,30 +150,41 @@ function updateTimer() {
  
 
 function startTimer() {
-  // TODO: Write your code here
+  //console.log("startTimer");
+  //starts game
   timer = setInterval(updateTimer, 1000);
   return timer;
 }
 
 
+
 function whack(event) {
-  // TODO: Write your code here.
- console.log("whack!");
+  console.log("whack");
+  // call updateScore();
+  // return points;
+ 
   updateScore();
  
 }
 
 function setEventListeners(){
-  // TODO: Write your code here
+  
+  // forEach mole add the whack event handler when a player clicks on the mole.
+  // return moles;
+  startButton.addEventListener('click', startGame);
   for (let mole of moles) {
     mole.addEventListener('click', whack);
   }
 
+  //console.log("setEventListeners");
+
   return moles;
+
 }
 
 
 function setDuration(duration) {
+  //console.log("setDuration");
   time = duration;
   timerDisplay.textContent = time;
   return time;
@@ -142,6 +192,7 @@ function setDuration(duration) {
 
 
 function stopGame(){
+  //console.log("stopGame");
   // stopAudio(song);  //optional
   clearInterval(timer);
   return "game stopped";
@@ -149,11 +200,16 @@ function stopGame(){
 
 
 function startGame(){
+ // console.log("startGame")
   setDuration(10);
   startTimer()
   showUp();
   return "game started";
 }
+
+
+setEventListeners();
+
 //function above provided
 
 // Please do not modify the code below.
